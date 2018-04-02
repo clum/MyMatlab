400f2ae973b27313275feb4533670da4545162a8
Christopher Lum <lum@u.washington.edu>
2018-04-01 22:44:09 -0700

    Added a subtraction function and changed some documentation on another file.

--- MyAdditionFunction.m
+++ MyAdditionFunction.m
@@ -1,6 +1,4 @@
 function [z] = MyAdditionFunction(x,y)
 
 %This function simply adds two numbers together
-%
-%I am adding a comment to this file.
 z = x + y;
---
+++ MySubtractionFunction.m
@@ -0,0 +1,5 @@
+function [z] = MySubtractionFunction(x,y)
+
+%This function simply subtracts two numbers.
+
+z = x - y;
